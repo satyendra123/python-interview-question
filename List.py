594c153c4f18cd81b3ccc8ebe0bb07d3a1a0cfe5
satyendra123 <Singhsatyendra885@gmail.com>
2025-05-08 23:47:02 +0530

    Update List.py

--- List.py
+++ List.py
@@ -3,17 +3,18 @@ l1=[1,2,3,"english","mango"]
 for i in l1:
    print(i)          o/p- 1 2 3 english mango
 
-EX-2
-data = "Hello How are you"
-vowels=[]
-consonent=[]
-vowel="AEIOUaeiou"
-for i in data:
-    if(i ==' '):
-        continue
-    elif(i in vowel):
-        vowels.append(i)
-    else:
+EX-2 counting the total vowels and consonent     EX-3                                          
+data = "Hello How are you"                       data = "Hello How are you"
+vowels=[]                                        capitalize_word=[]
+consonent=[]                                     words= data.split(' ')
+vowel="AEIOUaeiou"                               for word in words:
+for i in data:                                       capitalize = word[0].upper() + word[1:]
+    if(i ==' '):                                     capitalize_word.append(capitalize)
+        continue                                  result=''
+    elif(i in vowel):                             for word in capitalize_word:   
+        vowels.append(i)                              result+= word+' '
+    else:                                          print(result)
         consonent.append(i)
 print(vowels)
 print(consonent)
+
