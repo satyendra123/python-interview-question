209af972c899d261fb601500402b7eba215d7efa
satyendra123 <Singhsatyendra885@gmail.com>
2025-05-08 21:33:32 +0530

    Update List.py

--- List.py
+++ List.py
@@ -4,3 +4,16 @@ for i in l1:
    print(i)          o/p- 1 2 3 english mango
 
 EX-2
+data = "Hello How are you"
+vowels=[]
+consonent=[]
+vowel="AEIOUaeiou"
+for i in data:
+    if(i ==' '):
+        continue
+    elif(i in vowel):
+        vowels.append(i)
+    else:
+        consonent.append(i)
+print(vowels)
+print(consonent)
